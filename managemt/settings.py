bc93bfbe3d2bb43ab9d7362997e11b4a944a1385
kaan <akaanug@gmail.com>
2020-08-01 12:34:49 +0300

    add templates dir

--- managemt/settings.py
+++ managemt/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'managemt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [os.path.join(BASE_DIR, "Templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
