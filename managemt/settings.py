7d12b2402ffeb455ddff64de0a18e787f412505f
kaan <akaanug@gmail.com>
2020-08-01 12:22:49 +0300

    try new settings

--- managemt/settings.py
+++ managemt/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'managemt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
