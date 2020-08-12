efcc7ed071f584d2329a223f5619a1457a365fe9
kaan <akaanug@gmail.com>
2020-08-12 12:15:01 +0300

    Add quagga for barcode reading and remove cv2.

--- accounts/views.py
+++ accounts/views.py
@@ -25,8 +25,6 @@ from simple_history.utils import update_change_reason
 
 import pytz
 
-import cv2
-from pyzbar import pyzbar
 
 """
 @unauthenticated_user
@@ -171,7 +169,6 @@ def getHistoryLabel(product):
         "invoice": "Fatura",
         "history": "Tarih",
     }
-
 
     histories = product.history.all()
 
@@ -518,29 +515,7 @@ def history(request, pk):
     return render( request, 'accounts/history.html', context )
 
 
-
-def read_barcodes(frame):
-    barcodes = pyzbar.decode(frame)
-    for barcode in barcodes:
-        x, y , w, h = barcode.rect
-        barcode_text = barcode.data.decode('utf-8')
-        print(barcode_text)
-        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
-    return frame
-
-def read_barcode_main():
-    camera = cv2.VideoCapture(0)
-    ret, frame = camera.read()
-    while ret:
-        ret, frame = camera.read()
-        frame = read_barcodes(frame)
-        cv2.imshow('Barcode reader', frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
-    camera.release()
-    cv2.destroyAllWindows()
-
-
+@login_required(login_url='login')
 def barcodeView(request):
-    read_barcode_main()
+    context = {}
+    return render(request, 'accounts/readBarcode.html', context)
